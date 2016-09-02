199c9193be8cd7a54f2bccd40b04cff1108010d0
chrisg <cg94301@gmx.com>
2016-09-01 19:50:08 -0700

    revert to default setup

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -18,36 +18,11 @@ class LearningAgent(Agent):
         self.alpha = 0.5
         self.gamma = 0.5
         self.epsilon = 1.0
+        self.floor = 0.05
         self.log = False
-        #self.qd = {}
 
-        # States
-        self.states = ['greenforward','greenleft','greenright','redforward','redleft','redright']
-
-        # Q matrix, initialized to 0.0
-#        self.q = {'stop': pd.Series(.0, index = self.states),
-#                  'forward': pd.Series(.0, index = self.states),
-#                  'left': pd.Series(.0, index = self.states),
-#                  'right': pd.Series(.0, index = self.states)}
-#
-#
-        self.qinit = {'stop': .0,
-                  'forward': .0,
-                  'left': .0,
-                  'right': .0}
-        #self.qdf2 = pd.DataFrame(self.qinit, index = ['init'])
-        self.qdf2 = pd.DataFrame()
-
-        #self.qdf = pd.DataFrame(self.qinit, index = ['init'])
+        self.qinit = {'wait': .0, 'forward': .0, 'left': .0, 'right': .0}
         self.qdf = pd.DataFrame()
-
-        # Q* for debug purposes hardcoded
-        self.qbest = {'stop': pd.Series([1.0,1.0,1.0,1.0,1.0,1.0], index = self.states),
-                      'forward': pd.Series([3.0,0.5,0.5,0.0,0.0,0.0], index = self.states),
-                      'left': pd.Series([0.5,3.0,0.5,0.0,0.0,0.0], index = self.states),
-                      'right': pd.Series([0.5,0.5,3.0,0.5,0.5,3.0], index = self.states)}
-        self.qbestdf = pd.DataFrame(self.qbest)
-        
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -62,26 +37,34 @@ class LearningAgent(Agent):
 
         # TODO: Update state
 
-        #state2 = (inputs['light'], inputs['oncoming'], inputs['left'], inputs['right'], self.next_waypoint)
-        state2 = (inputs['light'], self.next_waypoint)
+        # Filter state space
+        # Can ignore traffic when red light except for potential collision on right turn on red
+        # Reason: Crossing red and accident have same penalty
+        if inputs['light'] == 'red':
+            if inputs['oncoming'] != 'left':
+                inputs['oncoming'] = None
+            if inputs['left'] != 'forward':
+                inputs['left'] = None
+            inputs['right'] = None
 
-        #state = inputs['light'] + self.next_waypoint
-        state = ':'.join((inputs['light'], self.next_waypoint))
-        #state3 = inputs['light'] + str(inputs['oncoming']) + str(inputs['left']) + str(inputs['right']) + self.next_waypoint
-        state3 = ':'.join((inputs['light'], str(inputs['oncoming']), str(inputs['left']), str(inputs['right']), self.next_waypoint))
+        if inputs['light'] == 'green':
+            # ignore oncoming right turn
+            if inputs['oncoming'] == 'right':
+                inputs['oncoming'] = None
+            # ignore right turn from left
+            if inputs['left'] == 'right':
+                inputs['left'] = None
+
+        # Select State Space
+        #state = ':'.join((inputs['light'], self.next_waypoint))
+        state = ':'.join((inputs['light'], str(inputs['oncoming']), str(inputs['left']), str(inputs['right']), self.next_waypoint))
+
         self.state = state
-        if self.log: print "[DEBUG] state: %s" % state
-
-        if state3 not in self.qdf2.index:
-            newq2 = pd.DataFrame(self.qinit, index = [state3])
-            self.qdf2 = self.qdf2.append(newq2)
+        if self.log: print "[DEBUG]: %s" % state
 
         if state not in self.qdf.index:
             newq = pd.DataFrame(self.qinit, index = [state])
             self.qdf = self.qdf.append(newq)
-
-        #print len(self.qdf2)
-        #print self.qdf2
 
         # TODO: Select action according to your policy
 
@@ -98,52 +81,40 @@ class LearningAgent(Agent):
             action = randaction
             if self.log: print "[DEBUG] randomaction: %s" % action
         else:
-            if qaction == 'stop':
+            if qaction == 'wait':
                 action = None
             else:
                 action = qaction
             if self.log: print "[DEBUG] bestaction: %s" % action
 
         # Slowly decrease epsilon, leave at 5% floor
-        self.epsilon -= .001
-        if self.epsilon <= .05:
-            self.epsilon = .05
+        if self.epsilon > self.floor:
+            self.epsilon -= .00075
 
-        # Override action w/ Q* for debug only
-        # qbestaction =  self.qbestdf.loc[state].idxmax()
-        # if qbestaction == 'stop':
-        #     qbestaction = None
-        # action = qbestaction
-        action = randaction
+        # Override selection. DEBUG only !
+        #action = qaction
+        #action = randaction
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
         if action == None:
-            nowstate = self.qdf['stop'][state]
+            nowstate = self.qdf['wait'][state]
         else:
             nowstate = self.qdf[action][state]
 
         nextstate = nowstate * ( 1 - self.alpha ) + self.alpha * ( reward + self.gamma * 2.0)
         if self.log: print "[DEBUG] nextstate: %s" % nextstate
 
-        # Update Q dict
-        #self.qd[(state2, action)] = (1 - self.alpha) * self.qd.get((state2, action), 0) + self.alpha * reward
-        #print "[DEBUG] qd:"
-        #print len(self.qd)
-        #print self.qd
-
-
         # Update Q matrix
         if action == None:
-            self.qdf['stop'][state] = nextstate
+            self.qdf['wait'][state] = nextstate
         else:
             self.qdf[action][state] = nextstate
 
         if self.log: print "[DEBUG] qdf:"
         if self.log: print self.qdf
-        print self.qdf
         
         self.env.totalreward += reward
         if self.log: print "[DEBUG] totalreward: %s" % self.env.totalreward
@@ -156,16 +127,18 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.1, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
+    print "Number of states reached %s" % len(a.qdf)
+    print a.qdf
 
 if __name__ == '__main__':
     run()
